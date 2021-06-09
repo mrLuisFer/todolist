854c74317e5f15524750be2c8afbd5cde660d9d4
mrLuisFer <lolesuncrak@gmail.com>
2021-06-09 00:00:39 -0500

    fix: Changed all taskdashboard in a file
    NOTE: Its a really bad practice, but its just for test

--- src/components/TaksDashboard/TaskDashboardModal/TaskDashBoardModal.jsx
+++ src/components/TaksDashboard/TaskDashboardModal/TaskDashBoardModal.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import TaskDashboardDescriptionSection from './TaskDashboardDescriptionSection/'
-import TaskDashboardInputSection from './TaskDashboardInputSection'
-import TDAddTaskSection from './TDAddTaskSection/TDAddTaskSection'
+import {
+  TDInputInfo,
+  TDInput,
+  TDInputContainer,
+} from './TaskDashboardInputSection/TaskDashboardInputSection.styles'
+
+import {
+  TDTaskDescriptionContainer,
+  TDTaskDescription,
+  TDOpenDescriptionBtn,
+  TDCloseDescriptionBtn,
+} from './TaskDashboardDescriptionSection/TaskDashboardDescriptionSection.styles'
+
+import { taskColors } from './TDAddTaskSection/taskColors'
+
+import {
+  TDAddTaskSectionContainer,
+  TDAddTaskColorsContainer,
+  TDAddTaskColor,
+  TDAddTaskButton,
+} from './TDAddTaskSection/TDAddTaskSection.styles'
 
 import { TasksDashboardModalContainer, TDFullWidth } from './TaskDashboardModal.styles'
 
@@ -12,12 +30,103 @@ import { TasksDashboardModalContainer, TDFullWidth } from './TaskDashboardModal.
  * @returns JSX.Element
  */
 export default function TaskDashBoardModal({ closeModalFunc }) {
+  const handleEnterKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      console.log('enter')
+    }
+  }
+
+  const handleChangeTaskValue = (e) => {
+    console.log(e.target.value)
+  }
+
+  const [showDescription, setShowDescription] = useState(false)
+
+  const handleShowDescription = () => {
+    setShowDescription(true)
+  }
+
+  const handleCloseDescription = () => {
+    setShowDescription(false)
+  }
+
+  useEffect(() => {
+    setShowDescription(false)
+  }, [])
+
+  const handleChangeDescriptionValue = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
     <TasksDashboardModalContainer>
       <TDFullWidth>
-        <TaskDashboardInputSection />
-        <TaskDashboardDescriptionSection />
-        <TDAddTaskSection />
+        {/* * * * * * * * * * * * * * * * * * * * * * * * * *  * * * */}
+        <TDInputContainer>
+          <TDInput
+            type='text'
+            name='task-input'
+            id='task-input'
+            placeholder='Add task...'
+            required
+            autoComplete='off'
+            onKeyDown={handleEnterKeyDown}
+            value=''
+            onChange={handleChangeTaskValue}
+          />
+          <TDInputInfo
+            className='fas fa-info-circle'
+            title='Press Enter for add the task'
+          />
+        </TDInputContainer>
+        {/* * * * * * * * * * * * * * * * * * * * * * * * * *  * * * */}
+        {/* * * * * * * * * * * * * * * * * * * * * * * * * *  * * * */}
+        <div>
+          {showDescription ? (
+            ''
+          ) : (
+            <TDOpenDescriptionBtn onClick={handleShowDescription}>
+              Description?
+            </TDOpenDescriptionBtn>
+          )}
+          {showDescription ? (
+            <TDTaskDescriptionContainer>
+              <TDCloseDescriptionBtn onClick={handleCloseDescription}>
+                <i className='fas fa-times' title='Cancel Description' />
+              </TDCloseDescriptionBtn>
+              <TDTaskDescription
+                name='task-description'
+                id='task-description'
+                cols='20'
+                rows='4'
+                placeholder='Some description?'
+                value={''}
+                onChange={handleChangeDescriptionValue}
+              ></TDTaskDescription>
+            </TDTaskDescriptionContainer>
+          ) : (
+            ''
+          )}
+        </div>
+        {/* * * * * * * * * * * * * * * * * * * * * * * * * *  * * * */}
+        {/* * * * * * * * * * * * * * * * * * * * * * * * * *  * * * */}
+        <TDAddTaskSectionContainer>
+          <TDAddTaskColorsContainer>
+            {taskColors.map((taskColor) => (
+              <TDAddTaskColor
+                bgColor={taskColor.color}
+                key={taskColor.color}
+                title={taskColor.titleInfo}
+              />
+            ))}
+          </TDAddTaskColorsContainer>
+          <TDAddTaskButton type='button'>
+            <i className='far fa-plus-square' title='Add a task' />
+            Add Task
+          </TDAddTaskButton>
+        </TDAddTaskSectionContainer>
+        {/* * * * * * * * * * * * * * * * * * * * * * * * * *  * * * */}
+        {/* * * * * * * * * * * * * * * * * * * * * * * * * *  * * * */}
       </TDFullWidth>
     </TasksDashboardModalContainer>
   )
